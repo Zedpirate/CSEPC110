dd60d83eec4214a8faea433c6e8e4792d54790a7
Zedpirate <122525976+Zedpirate@users.noreply.github.com>
2025-02-05 16:08:52 -0300

    Update w04_number_game.py

--- w04_number_game.py
+++ w04_number_game.py
@@ -29,13 +29,30 @@ elif guess > magic_number:
 else:
   print("You guessed it!")
 
-magic_number = 6  
-  while guess != magic_number
+
+##Generates Random Number and gives hint in order to guess the number.
+magic_number = random.randint(1,100)  
+guess_count = 0
+
+guess = float(input("Guess a number?: "))
+guess_count = guess_count + 1
+continue_playing = "yes"
+
+while continue_playing == "yes":
+    while guess != magic_number:
+      if guess < magic_number:
+        print("Guess Higher")
+      elif guess > magic_number:
+        print("Guess Lower")
+      else:
+        print("You guessed it!")
+      guess = float(input("Guess a number?: "))
+      guess_count = guess_count + 1
+    print()
+    print(f"It took you {guess_count} guesses")  
+    playing = input("Would you like to play again (yes/no)?:  ")
+    continue_playing = playing.lower()
+    guess_count = 0
+    magic_number = random.randint(1,10)  
     guess = float(input("Guess a number?: "))
-print()
-    if guess < magic_number:
-  print("Guess Higher")
-    elif guess > magic_number:
-  print("Guess Lower")
-    else:
-  print("You guessed it!")
+print("Thanks for playing!")
