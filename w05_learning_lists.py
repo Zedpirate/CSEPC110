1693fa5193f66b4cdb1fef94f6d2aac4b43f4ecb
Zedpirate <122525976+Zedpirate@users.noreply.github.com>
2025-02-06 09:07:08 -0300

    Create w05_learning_lists.py

---
+++ w05_learning_lists.py
@@ -0,0 +1,25 @@
+#Helaman Menk
+#Week05 Leearning Activity
+
+#Using square brakets we can create lists in python [ ] 
+
+clients = []
+clients = ["John", "Mary"]
+clients = list()
+
+#Can't use the word "list" as a variable because it's a keyword
+#Considered good practice to use plural word for variable chose for list name i.e. clients not client; players not player
+
+#To add new clients to the list we use append method.
+
+clients = ["John", "Mary"]
+print(clients)
+clients.append("Lucas")
+clients.append("Lisa")
+print()
+print(clients)
+
+#Checking this part still
+clientes.append(input("What's your client's name?: ")
+print()
+print(clients)
