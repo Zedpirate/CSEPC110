49697cae2599f1a9272dc064c42ed3aed24f1e80
Zedpirate <helaman.menk@gmail.com>
2025-02-06 23:44:15 -0300

    Finished word puzzle

--- w04_project_wordpuzzle.py
+++ w04_project_wordpuzzle.py
@@ -1,9 +1,16 @@
 
 #Helaman Menk
 #Week04 Project Word Puzzle
+#Creativity I was able to make it work like a word puzzle game, including the hints with Uppercase and lowercase letters.
+#I found online on how to make random choices from a list and used it to make the game more interesting. I created index called secrets for the word to be chosen randomly from there.
 
-secret = "learn"
-guess_count = 1
+
+import random
+
+secrets = ["learn", "python", "coding", "programming", "data", "science", "machine", "learning"]
+secret = random.choice(secrets)
+
+guess_count = 0
 letter_count = len(secret)
 
 print("Welcome to the Word Puzzle Game!")
@@ -17,12 +24,23 @@ for hint in range(letter_count):
 print()
 print()
 
-guess = input("What is your guess?: ")
+guess = ""
 while guess != secret:
-    print("Try again!")
-    print()
     guess = input("What is your guess?: ")
     guess_count = guess_count + 1
+
+    if len(guess) == letter_count:
+        for i, letter in enumerate(guess):    
+            if letter == secret[i]:
+                print(letter.upper(), end=" ")
+            elif letter in secret:
+                print(letter, end=" ")
+            else:
+                print("_", end=" ")
+        print()
+    print("Try again!")
+    print()
+
 print("You guessed it!")
 print(f"It took you {guess_count} guesses")
 print()
