27c5852509b65b915a9215a1959e02c9409fd26a
Zedpirate <122525976+Zedpirate@users.noreply.github.com>
2025-02-13 10:58:06 -0300

    Considering Opening from txt file

--- wk06_team_activity.py
+++ wk06_team_activity.py
@@ -12,35 +12,51 @@ hr_system = [
 
 #iterate through each line of this file, gather the information from each field and display or take certain actions depending on the data
 
-
-
+with open("hr_system.txt") as hr_system:
+  for line in hr_system:
+        parts = line.split(" ")
+  
 
 #2 - Split the line into parts and change your display, so that it shows only the names (instead of the whole line).
-for data in hr_system:
-  each_part = data.split()
-  name = each_part[0]
-  person_id = each_part[1]
-  job_title = each_part[2]
-  salary = each_part[3]
-  print(name)
+with open("hr_system.txt") as hr_system:
+  for data in hr_system:
+    each_part = data.split(" ")
+    name = each_part[0]
+    person_id = each_part[1]
+    job_title = each_part[2]
+    salary = each_part[3]
+    print(name)
 
 #3 - For each line, get the name and the job title for each person, and display those to the screen.
-for data in hr_system:
-  each_part = data.split()
-  name = each_part[0]
-  person_id = each_part[1]
-  job_title = each_part[2]
-  salary = each_part[3]
-  print(f"Name: {name}, Title: {job_title}")
+with open("hr_system.txt") as hr_system:
+  for data in hr_system:
+    each_part = data.split()
+    name = each_part[0]
+    person_id = each_part[1]
+    job_title = each_part[2]
+    salary = each_part[3]
+    print(f"Name: {name}, Title: {job_title}")
 
 #Stretch Challenge
 #Display all four values in this format: name (ID: id_number), job_title - $salary
 
-for data in hr_system:
-  each_part = data.split()
-  name = each_part[0]
-  person_id = each_part[1]
-  job_title = each_part[2]
-  salary = each_part[3]
-  print(f"Name: {name}, Title: {job_title}")
+with open("hr_system.txt") as hr_system:
+  for data in hr_system:
+    each_part = data.split()
+    name = each_part[0]
+    person_id = each_part[1]
+    job_title = each_part[2]
+    salary = int(each_part[3])
+    salary = (salary/12)/2
+    if job_title == "Engineer":
+      salary = salary + 1000
+    print(f"{name} (ID: {person_id}), {job_title} - ${salary:.2f}")
+
+#2 - Instead of displaying the salary information, calculate and display a paycheck amount for the employee. Assume that they are paid twice a month.
+#Added salary = (salary/12)/2 before the Print statement to do this
+
+#3 - Change the program so that it generates bonuses for anyone who is an engineer. For each of these employees, add $1000 to their paycheck amount.
+ #if job_title == "Engineer":
+ #   salary = salary + 1000
+#This If statement ran right after salary, checked if job_title is "engineer" and would add 1000
 
