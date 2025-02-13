5c47baab90c5c1a59f72ba4b9cb2fb61406f1ed8
Zedpirate <122525976+Zedpirate@users.noreply.github.com>
2025-02-13 11:06:28 -0300

    Create wk6_project.py

---
+++ wk6_project.py
@@ -0,0 +1,15 @@
+#Helaman Menk
+
+#Milestone
+#Assignment
+#Download the dataset and write a Python program to analyze it to answer the following questions:
+#What is the year and country that has the lowest life expectancy in the dataset?
+#What is the year and country that has the highest life expectancy in the dataset?
+#Allow the user to type in a year, then, find the average life expectancy for that year. 
+#Then find the country with the minimum and the one with the maximum life expectancies for that year.
+
+#File Format:
+#Entity,Code,Year,Life expectancy (years)
+
+with life-expectancy.csv open as f:
+  
