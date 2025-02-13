0843e6b9c8089d4456dcc46363934eac3b3ae22a
Zedpirate <122525976+Zedpirate@users.noreply.github.com>
2025-02-13 08:49:41 -0300

    Update wk06_learning_files.py

--- wk06_learning_files.py
+++ wk06_learning_files.py
@@ -19,3 +19,76 @@ print(color_list)
 print(color_list[1].strip())
 
 "If add "," with parentheses inside the split() then it'll use commas as seperator to create the list
+
+
+#Part Two
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+numbers = [42, 25, 18, 83, 23, 85, 38, 2, 110]
+
+largest_so_far = numbers[0]
+
+for number in numbers:
+    if number > largest_so_far:
+        # This number is larger than the largest we had seen so far
+
+        # So it is now the largest we've seen
+        largest_so_far = number
+
+# Now, after the loop we can display it:
+print(f"The largest is: {largest_so_far}")
+
+
+shopping_cart = [
+    ["Chips", 2.99],
+    ["Bread", 2.50],
+    ["Milk", 3.19],
+    ["Ice Cream", 6.99],
+    ["Cheese", 5.99],
+    ["Candy bar", 0.99]
+]
+
+max_price = -1
+
+for item in shopping_cart:
+    price = item[1] # The price is the second part of the item
+
+    if price > max_price:
+        # This is the new max
+        max_price = price
+
+print(f"The maximum price is: {max_price}")
+
+
+max_price = -1
+max_product = "" # It doesn't matter what you set this to, it just needs to be declared
+
+for item in shopping_cart:
+    product_name = item[0] # Product name is the first part
+    price = item[1] 
+
+    if price > max_price:
+        # This is the new max
+        max_price = price
+
+        # Also save this product name as the max product
+        max_product = product_name
+
+print(f"The maximum price is: {max_price}")
+print(f"The product with the maximum price is: {max_product}")
+
+
+
+#Activity
+
+people = [
+    "Stephanie 36",
+    "John 29",
+    "Emily 24",
+    "Gretchen 54",
+    "Noah 12",
+    "Penelope 32",
+    "Michael 2",
+    "Jacob 10"
+]
